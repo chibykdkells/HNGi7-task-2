bf99730eb7493390a694c08a969747c0ef9e0938
Kelvin O Chibuikem <chigbatachibuike@gmail.com>
2020-06-05 20:35:26 +0100

    Update kelvinc-HNG-02959.js

--- scripts/kelvinc-HNG-02959.js
+++ scripts/kelvinc-HNG-02959.js
@@ -1,10 +1,10 @@
-let myName = "Kelvin O Chibuikem";
+let myName = "Kelvin Chibuikem";
 let myEmail = "chigbatachibuike@gmail.com";
 let myId = "HNG-02959";
 let myLang = "JavaScript";
 
 function myInternDetails() {
-    console.log(`Hello world, this is ${myName} with HNGi7 ID ${myId} and email ${myEmail} using ${myLang} for stage 2 task`)
+    console.log(`Hello World, this is ${myName} with HNGi7 ID ${myId} and email ${myEmail} using ${myLang} for stage 2 task`)
 };
 
 myInternDetails(); 

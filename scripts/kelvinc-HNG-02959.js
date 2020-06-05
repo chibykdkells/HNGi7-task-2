a93175e8d8c86013c1eeffeda9a08b4021f6ac74
Kelvin O Chibuikem <chigbatachibuike@gmail.com>
2020-06-05 20:02:08 +0100

    Update kelvinc-HNG-02959.js

--- scripts/kelvinc-HNG-02959.js
+++ scripts/kelvinc-HNG-02959.js
@@ -7,4 +7,4 @@ function intern() {
     console.log(`Hello world, this is ${name} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`)
 };
 
-console.log(intern());
+intern();
